3c09687039431c688cb615c8d00c78a2bac7e605
Edwin Chu <edwin.chu@playnext.com>
2017-06-19 23:48:45 -0700

    Initial commit for helpers class

---
+++ src/main/java/com/sqa/ec/helpers/AppBasics.java
@@ -0,0 +1,22 @@
+package com.sqa.ec.helpers;
+
+import java.util.*;
+
+public class AppBasics
+{
+
+	private static Scanner scanner = new Scanner(System.in);
+
+	// Method to farewell user from the application.
+	public static void fareWellUser(String name)
+	{
+		System.out.println("\nThanks for using this app.\nHave a great day " + name + "!");
+	}
+
+	public static String greetUserAndGetName(String appName)
+	{
+		System.out.println("Welcome to the " + appName + " Application");
+		System.out.println("Could I get your name plaese: ");
+		return scanner.nextLine();
+	}
+}
