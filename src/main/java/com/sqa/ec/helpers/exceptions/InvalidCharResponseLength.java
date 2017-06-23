f757c1df3696e865891dd9e01b462f1500b7f627
Edwin Chu <edwin.chu@playnext.com>
2017-06-22 19:14:57 -0700

    Initial commit for exceptions

---
+++ src/main/java/com/sqa/ec/helpers/exceptions/InvalidCharResponseLength.java
@@ -0,0 +1,28 @@
+/**
+ *   File Name: InvalidCharResponseLength.java<br>
+ *
+ *   Chu, Edwin<br>
+ *   Java Boot Camp Exercise<br>
+ *   Instructor: Jean-francois Nepton<br>
+ *   Created: Jun 22, 2017
+ *   
+ */
+
+package com.sqa.ec.helpers.exceptions;
+
+
+/**
+ * InvalidCharResponseLength //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author      Chu, Edwin
+ * @version     1.0.0
+ * @since       1.0
+ *
+ */
+public class InvalidCharResponseLength extends Exception
+{
+}
