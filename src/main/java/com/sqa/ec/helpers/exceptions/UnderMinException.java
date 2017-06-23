88ff63d0196b98c61860f579a0cb67a36d0bdab4
Edwin Chu <edwin.chu@playnext.com>
2017-06-22 21:15:11 -0700

    add more exceptions files

---
+++ src/main/java/com/sqa/ec/helpers/exceptions/UnderMinException.java
@@ -0,0 +1,28 @@
+/**
+ *   File Name: UnderMinException.java<br>
+ *
+ *   Chu, Edwin<br>
+ *   Java Boot Camp Exercise<br>
+ *   Instructor: Jean-francois Nepton<br>
+ *   Created: Jun 22, 2017
+ *   
+ */
+
+package com.sqa.ec.helpers.exceptions;
+
+
+/**
+ * UnderMinException //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author      Chu, Edwin
+ * @version     1.0.0
+ * @since       1.0
+ *
+ */
+public class UnderMinException extends OutOfRangeException
+{
+}
