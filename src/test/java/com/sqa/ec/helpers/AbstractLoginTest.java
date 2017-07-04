a81b6989bb1f8b897b546c0416d61664c3a32c1c
Edwin Chu <edwin.chu@playnext.com>
2017-07-03 18:35:27 -0700

    update basic-project

---
+++ src/test/java/com/sqa/ec/helpers/AbstractLoginTest.java
@@ -0,0 +1,38 @@
+/**
+ * File Name: AbstractLoginTest.java<br>
+ * Chu, Edwin<br>
+ * Java Boot Camp Exercise<br>
+ * Instructor: Jean-francois Nepton<br>
+ * Created: Jul 1, 2017
+ */
+package com.sqa.ec.helpers;
+
+import com.sqa.ec.adactin.*;
+
+/**
+ * AbstractLoginTest //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ *
+ * @author Chu, Edwin
+ * @version 1.0.0
+ * @since 1.0
+ */
+public abstract class AbstractLoginTest extends BasicTest
+{
+
+	/**
+	 * @param baseUrl
+	 */
+	public AbstractLoginTest(String baseUrl)
+	{
+		super(baseUrl);
+		// TODO Auto-generated constructor stub
+	}
+
+	abstract public void login(String username, String password);
+
+	abstract public void logout();
+}
---
+++ src/test/java/com/sqa/ec/helpers/BasicTest.java
@@ -0,0 +1,60 @@
+package com.sqa.ec.helpers;
+
+import java.util.concurrent.*;
+
+import org.openqa.selenium.chrome.*;
+import org.openqa.selenium.firefox.*;
+import org.testng.annotations.*;
+
+import com.sqa.ec.auto.*;
+
+public class BasicTest extends Core
+{
+
+	public BasicTest(String baseUrl)
+	{
+		super(baseUrl);
+	}
+
+	@BeforeClass(enabled = true)
+	public void setUpChrome()
+	{
+		// Set system property to use Chrome driver
+		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+		// Setup the driver to use Chrome
+		System.out.println("Before Setting Chrome Driver");
+		setDriver(new ChromeDriver());
+		System.out.println("After setting Chrome Driver");
+		// Set the base URL for this test
+		// this.baseUrl = "https://www.amazon.com/";
+		// Set an implicit wait of up to 30 seconds
+		getDriver().manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		// Maximize the window
+		getDriver().manage().window().maximize();
+		getDriver().get(getBaseURL());
+	}
+
+	@BeforeClass(enabled = false)
+	public void setUpFirefox()
+	{
+		// Setup the driver to use Firefox
+		System.out.println("Before Setting FF Driver");
+		setDriver(new FirefoxDriver());
+		System.out.println("After setting FF Driver");
+		// Set the base URL for this test
+		// this.baseUrl = "https://www.amazon.com/";
+		// Set an implicit wait of up to 30 seconds
+		getDriver().manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		// Maximize the window
+		getDriver().manage().window().maximize();
+	}
+
+	@AfterClass
+	public void tearDown()
+	{
+		// quit closes all instances of the browser.
+		getDriver().quit();
+		// close method would close current instance only.
+		// ex: driver.close();
+	}
+}
