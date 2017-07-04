97aa2dffe85d8eb93aa36ee79d0d32909db507f6
Edwin Chu <edwin.chu@playnext.com>
2017-07-03 18:32:48 -0700

    update basic-project

---
+++ src/main/java/com/sqa/ec/helpers/enums/DataType.java
@@ -0,0 +1,24 @@
+/**
+ * File Name: DataType.java<br>
+ * Chu, Edwin<br>
+ * Java Boot Camp Exercise<br>
+ * Instructor: Jean-francois Nepton<br>
+ * Created: Jul 1, 2017
+ */
+package com.sqa.ec.helpers.enums;
+
+/**
+ * DataType //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ *
+ * @author Chu, Edwin
+ * @version 1.0.0
+ * @since 1.0
+ */
+public enum DataType
+{
+	STRING, INT, FLOAT, BOOLEAN, DOUBLE, CHAR
+}
---
+++ src/main/java/com/sqa/ec/helpers/exceptions/DataTypesCountException.java
@@ -0,0 +1,28 @@
+/**
+ *   File Name: DataTypesCountException.java<br>
+ *
+ *   Chu, Edwin<br>
+ *   Java Boot Camp Exercise<br>
+ *   Instructor: Jean-francois Nepton<br>
+ *   Created: Jul 1, 2017
+ *   
+ */
+
+package com.sqa.ec.helpers.exceptions;
+
+
+/**
+ * DataTypesCountException //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author      Chu, Edwin
+ * @version     1.0.0
+ * @since       1.0
+ *
+ */
+public class DataTypesCountException extends Exception
+{
+}
